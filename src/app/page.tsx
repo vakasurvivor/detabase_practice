e32e55c89694ac73f447cc0406ae188412dc77c6
vakasurvivor <110611464+vakasurvivor@users.noreply.github.com>
2025-02-13 01:57:22 +0900

    T3 Env を設定

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import { HelloUniverseLabel } from "@/components/hello-universe-label";
 
+// import { env } from "@/env";
+
 export default function Home() {
+  // const url = env.DEBUG_URL;
   return (
     <main>
       <div className="flex h-screen items-center justify-center">
         <div className="flex flex-col items-center">
           <HelloUniverseLabel />
+          {/* <p className="text-sm text-gray-500">{url}</p> */}
         </div>
       </div>
     </main>
