fc656000eb50d53c38590a178ab8fd6c1681aa9d
vakasurvivor <110611464+vakasurvivor@users.noreply.github.com>
2025-02-12 23:31:30 +0900

    Prettier を設定

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { HelloWorldLabel } from "@/components/hello-world-label";
 export default function Home() {
   return (
     <main>
-      <div className="flex justify-center items-center h-screen">
+      <div className="flex h-screen items-center justify-center">
         <div className="flex flex-col items-center">
           <HelloWorldLabel />
         </div>
